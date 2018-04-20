2c003237e315f874c1fc415046b2a36e5cf2f23c
Denis Lavrov <bahus.vel@gmail.com>
2018-04-20 13:27:09 +1200

    CLI arguments are working

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE RecordWildCards    #-}
 {-# OPTIONS_GHC -fno-cse #-}
 
 module Main where
@@ -6,29 +7,37 @@ module Main where
 import           Foreign
 import           Network.Pcap
 import           System.Console.CmdArgs
+import qualified System.IO              as SIO
 
 data TcpGraph = TcpGraph {
     iface  :: String,
-    period :: Int
+    period :: Int,
+    filt   :: [String]
 } deriving (Show, Data, Typeable)
 
 tcpGraph = TcpGraph {
     iface = def &= name "i",
-    period = def
+    period = def,
+    filt = def &= args
 }
+
+printErr = SIO.hPutStr SIO.stderr
 
 main :: IO ()
 main = do
-    args <- cmdArgs tcpGraph
-    print args
-    p <- openLive "enp0s31f6" 1500 True 1000
-    setFilter p "icmp" True 0
+    TcpGraph {..} <- cmdArgs tcpGraph
+    dev <- if iface == ""
+        then findDev
+        else return iface
+    p <- openLive dev 1500 True 1000
+    let f = foldr (\x y -> concat [x, " ", y]) "" filt
+    setFilter p f True 0
     loop p (-1) printIt
     return ()
 
 printIt :: PktHdr -> Ptr Word8 -> IO ()
-printIt ph bytep =
-    peekArray (fromIntegral (hdrCaptureLength ph)) bytep >>= print
+printIt ph bytep = printErr "."
+    --peekArray (fromIntegral (hdrCaptureLength ph)) bytep >>= print
 
 findDev :: IO String
 findDev = ifName . head <$> findAllDevs
