24c57370bfbb01283bb6d6c786c0cebdf60cf76a
Denis Lavrov <bahus.vel@gmail.com>
2018-04-20 17:03:09 +1200

    Added packet size graph mode

--- app/Main.hs
+++ app/Main.hs
@@ -7,21 +7,30 @@ module Main where
 import           Foreign
 import           Network.Pcap
 import           System.Console.CmdArgs
-import qualified System.IO              as SIO
+import qualified System.Console.Terminal.Size as TSIZE
+import           System.Exit
+import qualified System.IO                    as SIO
+import qualified System.Posix                 as PO6
 
 data TcpGraph = TcpGraph {
     iface  :: String,
     period :: Int,
-    filt   :: [String]
+    filt   :: [String],
+    mode   :: String
 } deriving (Show, Data, Typeable)
+
+modePacketSize = "packetsize"
+maxPacketSize :: Double
+maxPacketSize = 1500.0
 
 tcpGraph = TcpGraph {
     iface = def &= name "i",
     period = def,
-    filt = def &= args
+    filt = def &= args,
+    mode = modePacketSize &= name "m"
 }
 
-printErr = SIO.hPutStr SIO.stderr
+printErr = SIO.hPutStrLn SIO.stderr
 
 main :: IO ()
 main = do
@@ -32,12 +41,23 @@ main = do
     p <- openLive dev 1500 True 1000
     let f = foldr (\x y -> concat [x, " ", y]) "" filt
     setFilter p f True 0
-    loop p (-1) printIt
+    loop p (-1) $ drawPacket mode
     return ()
 
-printIt :: PktHdr -> Ptr Word8 -> IO ()
-printIt ph bytep = printErr "."
+drawPacket :: String -> PktHdr -> Ptr Word8 -> IO ()
+drawPacket m ph _ | m == modePacketSize = dotsForSize $ fromIntegral (hdrCaptureLength ph)
+
     --peekArray (fromIntegral (hdrCaptureLength ph)) bytep >>= print
 
 findDev :: IO String
 findDev = ifName . head <$> findAllDevs
+
+
+dotsForSize :: Int -> IO ()
+dotsForSize n = do
+    window <- TSIZE.size
+    case window of
+        Just TSIZE.Window {..} -> do
+            let dots = round $ fromIntegral width * (fromIntegral n / maxPacketSize)
+            putStrLn $ concat $ replicate dots "*"
+        Nothing                -> printErr "Not a tty!" >> exitFailure
